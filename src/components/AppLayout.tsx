56253bf470b6d7347cf1f85b1e541927d1231849
Shizin <shizin8089@gmail.com>
2025-11-08 04:32:30 +0000

    add button to switch

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -45,6 +45,7 @@ import {
   DropdownMenuSubContent,
 } from './ui/dropdown-menu';
 import { useTheme } from 'next-themes';
+import { ThemeToggle } from './ThemeToggle';
 
 const navItems = [
   { href: '/home', icon: Home, label: 'Analyzer' },
@@ -92,6 +93,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
           </SidebarMenu>
         </SidebarContent>
         <SidebarFooter>
+          <div className="flex items-center gap-2 group-data-[collapsible=icon]:flex-col">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button
@@ -148,6 +150,10 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
+            <div className="hidden group-data-[collapsible=expanded]:block">
+              <ThemeToggle />
+            </div>
+          </div>
         </SidebarFooter>
       </Sidebar>
       <SidebarInset>
@@ -156,6 +162,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
           <div className="flex-1">
             <Logo />
           </div>
+          <ThemeToggle />
         </header>
         <main className="flex-1 overflow-auto p-4 sm:p-6">{children}</main>
       </SidebarInset>
